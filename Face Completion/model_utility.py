0a9a3b86d0dbb61766b9fdfc0009822600993154
Mayur Garg <mayur7garg@gmail.com>
2021-08-10 09:32:48 +0530

    Trained face completion model till 10 epochs

--- Face Completion/model_utility.py
+++ Face Completion/model_utility.py
@@ -9,7 +9,7 @@ IMAGE_SIZE = (192, 160)
 LANDMARKS = ['lefteye', 'righteye', 'nose', 'leftmouth', 'rightmouth']
 
 class LandmarkCutoutImageIterator(Sequence):
-    def __init__(self, df, batch_size, step_count, max_cutouts = 2, min_cutout_size = 12, max_cutout_size = 15):
+    def __init__(self, df, batch_size, step_count, max_cutouts = 2, min_cutout_size = 12, max_cutout_size = 16):
         self.df = df
         self.batch_size = batch_size
         self.step_count = step_count
@@ -25,9 +25,10 @@ class LandmarkCutoutImageIterator(Sequence):
         masks = []
         y = []
 
-        for _ in range(self.batch_size):
-            row = self.df.sample()
-            img = Image.open(os.path.join(IMG_BASE_PATH, row['image_id'].iloc[0]))
+        rows = self.df.sample(self.batch_size)
+
+        for i in range(self.batch_size):
+            img = Image.open(os.path.join(IMG_BASE_PATH, rows['image_id'].iloc[i]))
             w, h = img.size
             img = img.resize((IMAGE_SIZE[1], IMAGE_SIZE[0]))
             img = np.array(img)/255.0
@@ -38,8 +39,8 @@ class LandmarkCutoutImageIterator(Sequence):
             cutout_features = random.sample(LANDMARKS, cutout_count)
 
             for feature in cutout_features:
-                cutout_x = int((row[feature + '_x'].iloc[0]/w) * IMAGE_SIZE[1])
-                cutout_y = int((row[feature + '_y'].iloc[0]/h) * IMAGE_SIZE[0])
+                cutout_x = int((rows[feature + '_x'].iloc[i]/w) * IMAGE_SIZE[1])
+                cutout_y = int((rows[feature + '_y'].iloc[i]/h) * IMAGE_SIZE[0])
 
                 cutout_size = random.randint(self.min_cutout_size, self.max_cutout_size)
                 
@@ -52,3 +53,7 @@ class LandmarkCutoutImageIterator(Sequence):
             y.append(img)
 
         return ((np.array(x), np.array(masks)), np.array(y))
+
+    def __call__(self):
+        for i in range(self.step_count):
+            yield self.__getitem__(i)
